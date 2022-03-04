1e5f599a150615ddd62ac18d89469cf078f1ae4b
ZaranaCodeline <100285894+zaranacodeline@users.noreply.github.com>
2022-03-03 23:33:43 -0800

    push set project

---
+++ lib/Splesh_Screen/views/splesh.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Splesh extends StatelessWidget {
+  const Splesh({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
